370e67a41ffd1f8ea1ff4a43b1c1d2f6b53c45a4
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-06-05 10:19:50 +0530

    sai commit 1

--- DemoProject/src/Demo2.java
+++ DemoProject/src/Demo2.java
@@ -3,10 +3,10 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		System.out.println("Demo2 class");
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println(c);
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println(z);
 	}
 
 }
